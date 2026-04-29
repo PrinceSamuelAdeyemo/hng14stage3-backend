beeb266e14916e435cefb050a877b8a0e18067f6
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2026-04-29 18:22:15 +0100

    loaded the env

--- insighta_cli.py
+++ insighta_cli.py
@@ -10,6 +10,7 @@ import webbrowser
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from pathlib import Path
 from dotenv import load_dotenv
+load_dotenv()
 
 CREDENTIAL_PATH = Path.home() / ".insighta" / "credentials.json"
 
